1cd717ba3e378a6fdbc025dfeb85ae5b0c4118a5
Kenneth Reitz <me@kennethreitz.com>
2013-08-15 04:37:58 -0400

    basics

--- www.py
+++ www.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    return 'Hello World!'
+
+@app.route('/<string:profile>/')
+def get_profile():
+    return 'Hello World!'
+
+@app.route('/<string:profile>/<path:document>/')
+def get_profile():
     return 'Hello World!'
 
 if __name__ == '__main__':
