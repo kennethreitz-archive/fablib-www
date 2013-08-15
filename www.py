076c2b20578dac68cb1ccf6a46540c223fcf28cf
Kenneth Reitz <me@kennethreitz.com>
2013-08-15 04:46:44 -0400

    working

--- www.py
+++ www.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import requests
 from flask import Flask
+
+API_URL = os.environ['API_URL']
+
 app = Flask(__name__)
+app.debug = True
+s = requests.session()
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
 @app.route('/<string:profile>/')
-def get_profile():
+def get_profile(profile):
     return 'Hello World!'
 
 @app.route('/<string:profile>/<path:document>/')
-def get_profile():
-    return 'Hello World!'
+def get_document(profile, document):
+    url = '{}/{}/{}'.format(API_URL, profile, document)
+    r = s.get(url)
+    return r.content
 
 if __name__ == '__main__':
     app.run()
