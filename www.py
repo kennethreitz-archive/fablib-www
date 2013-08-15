537727edbb3042839c56c4dc983ef23741b6337a
Kenneth Reitz <me@kennethreitz.com>
2013-08-15 05:29:18 -0400

    markdown

--- www.py
+++ www.py
@@ -3,6 +3,7 @@
 import os
 
 import requests
+from markdown2 import markdown
 from flask import Flask, request, render_template
 
 API_URL = os.environ['API_URL']
@@ -13,19 +14,19 @@ s = requests.session()
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', text=markdown('yo.\n\n# bro'))
 
 @app.route('/<string:profile>')
 def get_profile(profile):
     url = '{}/{}/{}'.format(API_URL, profile, document)
     r = s.get(url)
-    return r.content
+    return render_template('index.html', text=markdown(r.content))
 
 @app.route('/<string:profile>/<path:document>')
 def get_document(profile, document):
     url = '{}/render/{}/{}'.format(API_URL, profile, document)
     r = s.get(url)
-    return r.content
+    return render_template('index.html', text=markdown(r.content))
 
 if __name__ == '__main__':
     app.run()
