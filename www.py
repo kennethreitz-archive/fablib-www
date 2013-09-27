1ddc6073a1233e1d2a37613bf335e26f181ada12
Kenneth Reitz <me@kennethreitz.com>
2013-09-27 05:17:30 -0400

    for now

--- www.py
+++ www.py
@@ -9,32 +9,28 @@ from flask import Flask, request, render_template
 API_URL = os.environ['API_URL']
 
 app = Flask(__name__)
+app.debug = True
 s = requests.session()
 
 @app.route('/')
 def hello():
+    return render_template('index.html', text=markdown('# Fallib: Documentation for Humans. \n## Users \n\n- [kennethreitz](/kennethreitz)'))
+
+@app.route('/')
+def login():
     return render_template('index.html', text=markdown('# Fallib\n\n## Document ALL the Things!'))
 
-@app.route('/<string:profile>')
-def get_profile(profile):
-    url = '{}/{}'.format(API_URL, profile)
+@app.route('/<path:document>')
+def get_document(document):
+    url = '{}/{}/html'.format(API_URL, document)
     r = s.get(url)
-    text = r.json()['document']['text']
-    return render_template('index.html', text=markdown(text))
-
-@app.route('/<string:profile>/<path:document>')
-def get_document(profile, document):
-    url = '{}/{}/{}'.format(API_URL, profile, document)
-    r = s.get(url)
-    text = r.json()['document']['text']
-    return render_template('index.html', text=markdown(text))
+    return render_template('index.html', text=r.text), r.status_code
 
 @app.route('/content/<path:document>')
 def get_content(document):
-    url = '{}/content/{}'.format(API_URL, document)
+    url = '{}/content/{}/text'.format(API_URL, document)
     r = s.get(url)
-    text = r.json()['text']
-    return render_template('index.html', text=markdown(text))
+    return render_template('index.html', text=r.text)
 
 if __name__ == '__main__':
     app.run()
